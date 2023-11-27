22c0f97be4806d4533991ddc1cacb365f6c03655
JOSE CARLOS DIAZ GUERRERO <jcdiazgu@correo.url.edu.gt>
2023-11-26 21:25:03 -0600

    P4

--- PAT_HojaDeTrabajo03/Ejercicio02.cpp
+++ PAT_HojaDeTrabajo03/Ejercicio02.cpp
@@ -4,32 +4,31 @@
 
 vector<vector<string>>* Ejercicio02::groupAnagrams(vector<string>& strings)
 {
-    //Vector vacio
     if (strings.empty()) {
         return new std::vector<std::vector<std::string>>;
     }
 
-    //Letra repetida
-    std::sort(strings.begin(), strings.end());
-    if (std::adjacent_find(strings.begin(), strings.end()) != strings.end()) {
-        return new std::vector<std::vector<std::string>>;
-    }
-
-
     std::unordered_map<std::string, std::vector<std::string>> anagramGroups;
 
-    // Recorrido
     for (const auto& letra : strings) {
         
         std::string sortedWord = letra;
         std::sort(sortedWord.begin(), sortedWord.end());
         anagramGroups[sortedWord].push_back(letra);
     }
-    auto result = new std::vector<std::vector<std::string>>;
+
+    auto result = new std::vector<std::vector<std::string>>(anagramGroups.size());
+    auto it = result->begin();
+    for (const auto& group : anagramGroups) {
+        *it = group.second;
+        ++it;
+    }
+    /* auto result = new std::vector<std::vector<std::string>>;
     
     for (const auto& group : anagramGroups) {
         result->push_back(group.second);
-    }
+    }*/
+   
 
     return result;
 }
